cabc423e32b8a8a11c1083779413575de2b2d071
Joseortiz0918 <145424169+Joseortiz0918@users.noreply.github.com>
2024-04-17 19:24:20 -0500

    added login route and log in page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App.jsx'
 import Home from './pages/home.jsx';
 import ErrorPage from './pages/error.jsx';
+import Login from './pages/login.jsx';
 import './index.css'
 
 const router = createBrowserRouter([
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Home />
+      },
+      {
+        path: '/login',
+        element: <Login />
       }
     ]
   }
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,9 +1,15 @@
 import MyCalendar from "../components/CalendarForm";
+import { Link } from "react-router-dom";
+
+
 export default function Home() {
     const events = []
     return (
         <div>
             <h1>Home Page</h1>
+            <Link to='./login'>
+                <button>Log in</button>
+            </Link>
             <MyCalendar myEventsList={events}/>
         </div>
     )
