3a0a3b1b3136977ac9d884066110813595bb591d
cpayment <cpayment12@gmail.com>
2024-04-17 21:35:46 -0500

    Created signup.jsx under pages folder and added ADD_USER to mutations.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -11,3 +11,24 @@ query matchups($_id: String) {
   }
 }
 `;
+
+export const ADD_USER = gql`
+  mutation addUser(
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
