6016d31cd928a181c9cc18da733679843ca1a97f
Joseortiz0918 <145424169+Joseortiz0918@users.noreply.github.com>
2024-04-18 19:13:07 -0500

    styled login page

--- client/src/components/loginForm/index.jsx
+++ client/src/components/loginForm/index.jsx
@@ -1,29 +1,16 @@
 const Login =()=>(
-<div>
-      <h1 className=''>Login</h1>
-      <form>
-        <input
-          Label="Email"
-          inputStyle="box"
-          placeholder="Enter your name"
-        ></input>
-        <input
-          Label="Email"
-          inputStyle="box"
-          placeholder="Enter your email"
-        ></input>
-        <input
-          Label="Email"
-          inputStyle="box"
-          placeholder="Enter your password"
-        ></input>
-        <input
-          Label="Email"
-          inputStyle="box"
-          placeholder="Re-enter your password"
-        ></input>
-        <button type="submit">Create Account</button>
-      </form>
+<div class="max-w-[280px] mx-auto">
+        <div class="flex flex-col items-center mt-[10vh]">
+            <h2 class="mb-5 text-gray-900 font-mono font-bold text-xl">Log In</h2>
+            <form>
+                <input type="text" class="w-full px-6 py-3 mb-2 border border-slate-600 rounded-lg font-medium " placeholder="Email"/>
+                <input type="password" class="w-full px-6 py-3 mb-2 border border-slate-600 rounded-lg font-medium " placeholder="Password" />
+                <button class="bg-slate-500 hover:bg-slate-700 text-white text-base rounded-lg py-2.5 px-5 transition-colors w-full text-[19px]">Log In</button>
+            </form>
+            <p class="text-center mt-3 text-[14px]">Don&#x27;t have an account? 
+                <a href="/signup" class="text-gray-600">Create one</a>
+            </p>
+        </div>
     </div>
 )
 
