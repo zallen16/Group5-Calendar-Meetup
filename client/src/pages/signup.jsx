4fe7e1b92e8008802e61995b064ebad4929ac1f6
cpayment <cpayment12@gmail.com>
2024-04-18 20:54:43 -0500

    Styled Signup page

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -31,55 +31,56 @@ function Signup(props) {
     };
   
     return (
-      <div className="container my-1">
-        <Link to="/login">← Go to Login</Link>
-  
-        <h2>Signup</h2>
-        <form onSubmit={handleFormSubmit}>
-          <div className="flex-row space-between my-2">
-            <label htmlFor="firstName">First Name:</label>
-            <input
-              placeholder="First"
-              name="firstName"
-              type="firstName"
-              id="firstName"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex-row space-between my-2">
-            <label htmlFor="lastName">Last Name:</label>
-            <input
-              placeholder="Last"
-              name="lastName"
-              type="lastName"
-              id="lastName"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex-row space-between my-2">
-            <label htmlFor="email">Email:</label>
-            <input
-              placeholder="youremail@test.com"
-              name="email"
-              type="email"
-              id="email"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex-row space-between my-2">
-            <label htmlFor="pwd">Password:</label>
-            <input
-              placeholder="******"
-              name="password"
-              type="password"
-              id="pwd"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex-row flex-end">
-            <button type="submit">Submit</button>
-          </div>
-        </form>
+      <div className="max-w-[280px] mx-auto">
+        <div className="flex flex-col items-center mt-[10vh]">
+          <h2 className="mb-5 text-gray-900 font-mono font-bold text-xl">Signup</h2>
+          <form onSubmit={handleFormSubmit}>
+            <div className="w-full px-6 py-3 mb-2 border border-slate-600 rounded-lg font-medium ">
+              {/* <label htmlFor="firstName">First Name:</label> */}
+              <input
+                placeholder="First Name"
+                name="firstName"
+                type="firstName"
+                id="firstName"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="w-full px-6 py-3 mb-2 border border-slate-600 rounded-lg font-medium ">
+              {/* <label htmlFor="lastName">Last Name:</label> */}
+              <input
+                placeholder="Last Name"
+                name="lastName"
+                type="lastName"
+                id="lastName"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="w-full px-6 py-3 mb-2 border border-slate-600 rounded-lg font-medium ">
+              {/* <label htmlFor="email">Email:</label> */}
+              <input
+                placeholder="Email"
+                name="email"
+                type="email"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="w-full px-6 py-3 mb-2 border border-slate-600 rounded-lg font-medium ">
+              {/* <label htmlFor="pwd">Password:</label> */}
+              <input
+                placeholder="Password"
+                name="password"
+                type="password"
+                id="pwd"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="flex-row flex-end">
+              <button type="submit" className="bg-slate-500 hover:bg-slate-700 text-white text-base rounded-lg py-2.5 px-5 transition-colors w-full text-[19px]">Submit</button>
+            </div>
+          </form>
+          <Link to="/login">← Go to Login</Link>
+        </div>
       </div>
     );
   }
