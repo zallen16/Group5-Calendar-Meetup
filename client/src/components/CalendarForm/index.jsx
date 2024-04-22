e99c71f1a57a46333ba663624c945985374f93ac
zallen16 <126983111+zallen16@users.noreply.github.com>
2024-04-22 02:25:59 -0400

    created delete jsx file and added to calendar comp

--- client/src/components/CalendarForm/index.jsx
+++ client/src/components/CalendarForm/index.jsx
@@ -3,6 +3,8 @@ import { Calendar, dayjsLocalizer } from 'react-big-calendar'
 import { useState } from 'react';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 // import 'react-big-calendar/lib/addons/dragAndDrop/styles';
+import { useMutation } from '@apollo/client';
+import { DELETE_EVENT } from '../../utils/mutations';
 
 
 const localizer = dayjsLocalizer(dayjs)
@@ -27,6 +29,20 @@ const MyCalendar = (props) => {
       ]);
   };
 
+  const [deleteEvent] = useMutation(DELETE_EVENT);
+
+  const handleDeleteEvent = async (eventId) => {
+    try {
+      
+      setEvents(events.filter(event => event.id !== eventId));
+      
+      await deleteEvent({ variables: { eventId } });
+    } catch (error) {
+      // Handle error
+      console.error('Error deleting event:', error);
+    }
+  };
+
   return (
   <div>
     <Calendar
@@ -38,8 +54,12 @@ const MyCalendar = (props) => {
       startAccessor="start"
       endAccessor="end"
       style={{ height: 500 }}
+      {...events.map(event => (
+        <removeEvent key={event.id} id={event.id} title={event.title} onDelete={handleDeleteEvent} />
+      ))}
     />
   </div>
+  
 )}
 
 export default MyCalendar;
